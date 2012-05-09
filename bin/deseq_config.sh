a523b7e1976c335b8d345508e5ef6f26f72499bd
vipints <vipin@cbio.mskcc.org>
2012-05-09 19:05:11 -0400

    bin directory added

---
+++ bin/deseq_config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (C) 2010-2012 Max Planck Society
+export DESEQ_VERSION=1.6.0
+export DESEQ_PATH=
+export DESEQ_SRC_PATH=$DESEQ_PATH/src
+export DESEQ_BIN_PATH=$DESEQ_PATH/bin
+export INTERPRETER=
+export MATLAB_BIN_PATH=
+export MATLAB_MEX_PATH=
+export MATLAB_INCLUDE_DIR=
+export OCTAVE_BIN_PATH=
+export OCTAVE_MKOCT=
+export SAMTOOLS_DIR=
+export PYTHON_PATH=
+export SCIPY_PATH=
+export R_PATH=
+export LD_LIBRARY_PATH=
+export ENVIRONMENT=galaxy
